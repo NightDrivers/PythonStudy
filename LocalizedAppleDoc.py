1e6e9c5aa9bf337e9345804be4748cea0d677aac
NightDriver <lin_de_chun@sina.com>
2019-12-12 16:59:57 +0800

    修复多行注释中，最后一个语言的注释无法正常读取的问题

--- LocalizedAppleDoc.py
+++ LocalizedAppleDoc.py
@@ -4,29 +4,42 @@ import glob
 import os
 import argparse
 import shutil
+import cProfile
+
+
+def re_replace(content_text, occurrence_rex, content_rex):
+    source = content_text
+    searchpos = 0
+    while True:
+        occurrence_match = occurrence_rex.search(source, pos=searchpos)
+        if occurrence_match is None:
+            break
+        else:
+            occurrence_text = occurrence_match.group(1)
+            # 当content_rex中最后一个分组使用"$"时，而且原occurrence_text中最后一个字符是"\n"时，这个"\n"无法匹配到
+            # 暂时添加"\n"字符抵消这个情况
+            content_match = content_rex.search(occurrence_text + "\n")
+            if content_match is None:
+                searchpos = occurrence_match.end(0)
+                continue
+            else:
+                source = source[:occurrence_match.start(1)] + content_match.group(1) + source[occurrence_match.end(1):]
+                searchpos = occurrence_match.end(0) - len(occurrence_text) + len(content_match.group(1))
+    return source
 
 
 def convert_comment(filepath, lang):
     with open(filepath, "rt+") as file:
         text = file.read()
-        # 多行注释 支持/* */ /*! */ /** */ 注释其他行必须" *  "开头
-        muti_line_comment_rex = "/\\*[\\*!]?\n(.*?) \\*/"
-        muti_line_content_rex = "[^\n]*\\\\~" + lang + "\n(.*?)" + "([^\n]*\\\\~| \\*/)"
-        muti_line_comments = re.findall(muti_line_comment_rex, text, re.RegexFlag.DOTALL)
-        for muti_line_comment in muti_line_comments:
-            results = re.findall(muti_line_content_rex, muti_line_comment, re.RegexFlag.DOTALL)
-            if len(results) > 0:
-                result = results[0]
-                text = text.replace(muti_line_comment, result[0], 1)
+        # 多行注释 支持/* */ /*! */ /** */
+        multi_line_comment_re = re.compile("/\\*[\\*!]?\n(.*?) \\*/", re.RegexFlag.DOTALL)
+        multi_line_content_rex = "[^\n]*\\\\~" + lang + "\n(.*?)" + "([^\n]*\\\\~|$)"
+        multi_line_content_re = re.compile(multi_line_content_rex, re.RegexFlag.DOTALL)
+        text = re_replace(text, multi_line_comment_re, multi_line_content_re)
         # 单行注释 ///< //!< 格式
-        single_line_comment_rex = "//[/!]< *(.*?)\n"
-        single_line_content_rex = "\\\\~" + lang + " *(.*?)( *\\\\~|\n)"
-        single_line_comments = re.findall(single_line_comment_rex, text, re.RegexFlag.DOTALL)
-        for single_line_comment in single_line_comments:
-            results = re.findall(single_line_content_rex, single_line_comment, re.RegexFlag.DOTALL)
-            if len(results) > 0:
-                result = results[0]
-                text = text.replace(single_line_comment, result[0], 1)
+        single_line_comment_rex = re.compile("//[/!]< *(.*?)\n", re.RegexFlag.DOTALL)
+        single_line_content_rex = re.compile("\\\\~" + lang + " *(.*?)( *\\\\~|$)", re.RegexFlag.DOTALL)
+        text = re_replace(text, single_line_comment_rex, single_line_content_rex)
         file.seek(0, 0)
         file.truncate(len(text))
         file.write(text)
@@ -38,10 +51,10 @@ def parse_argv():
     return __parse.parse_args()
 
 
-if __name__ == '__main__':
-    parser = parse_argv()
-    framework_source_path = parser.framework
-    # framework_source_path = "/Users/ldc/Desktop/Demo/Doc/Doc.framework"
+def main():
+    # parser = parse_argv()
+    # framework_source_path = parser.framework
+    framework_source_path = "/Users/ldc/Desktop/外发文件/Hipos/HiPosExample/HiPosSDK.framework"
     langs = ["chinese", "english"]
     company_name = "hanin"
     company_id = "com.hanin"
@@ -54,17 +67,17 @@ if __name__ == '__main__':
     os.mkdir(sdkpath)
 
     for lang in langs:
-        langDir = "sdk/" + lang
-        if os.path.exists(langDir):
-            shutil.rmtree(langDir)
-        os.mkdir(langDir)
-        frameworkpath = langDir + "/" + framework
+        langdir = "sdk/" + lang
+        if os.path.exists(langdir):
+            shutil.rmtree(langdir)
+        os.mkdir(langdir)
+        frameworkpath = langdir + "/" + framework
         shutil.copytree(framework_source_path, frameworkpath)
         paths = glob.iglob(frameworkpath + "/**/*.h", recursive=True)
         # print(paths)
         for path in paths:
             convert_comment(path, lang)
-        cmd = "cd " + langDir + ";"
+        cmd = "cd " + langdir + ";"
         cmd += "appledoc"
         cmd += " --project-name " + project_name
         cmd += " --project-company " + company_name
@@ -72,4 +85,9 @@ if __name__ == '__main__':
         cmd += " --create-html --no-create-docset --no-install-docset --no-publish-docset"
         cmd += " --output ./ " + framework + ";"
         os.system(cmd)
-        os.rename(langDir + "/html", langDir + "/doc")
+        os.rename(langdir + "/html", langdir + "/doc")
+
+
+if __name__ == '__main__':
+    # cProfile.run("main()")
+    main()
--- test.py
+++ test.py
@@ -2,8 +2,40 @@
 import re
 
 
+def re_replace(content_text, occurrence_rex, content_rex):
+    source = content_text
+    searchpos = 0
+    while True:
+        occurrence_match = occurrence_rex.search(source, pos=searchpos)
+        if occurrence_match is None:
+            break
+        else:
+            occurrence_text = occurrence_match.group(1)
+            # print(result)
+            content_match = content_rex.search(occurrence_text)
+            if content_match is None:
+                print("not match content")
+                searchpos = occurrence_match.end(0)
+                continue
+            else:
+                print(repr(content_match.group(1)))
+                print("--------")
+                source = source[:occurrence_match.start(1)] + content_match.group(1) + source[occurrence_match.end(1):]
+                searchpos = occurrence_match.end(0) - len(occurrence_text) + len(content_match.group(1))
+    return source
+
+
 if __name__ == '__main__':
-    rex = "[^\\d]+"
-    a = "ab2bc34werqwe1232"
-    b = re.findall(rex, a, re.RegexFlag.DOTALL)
-    print(b)
+    a = "abcdefg"
+    rex = "a(?:(ef)|(b(c)))(?:d)"
+    m = re.search(rex, a)
+    print(m.group(0))
+    print(m.group(1))
+    print(m.group(2))
+    print(m.group(3))
+    with open("/Users/ldc/Desktop/外发文件/Hipos/HiPosExample/HiPosSDK.framework/Headers/HiPosPrinter.h", "rt+") as file:
+        text = file.read()
+        multi_line_comment_re = re.compile("/\\*[\\*!]?\n(.*?) \\*/", re.RegexFlag.DOTALL)
+        multi_line_content_rex = "[^\n]*\\\\~" + "english" + "\n(.*?)" + "([^\n]*\\\\~|$)"
+        multi_line_content_re = re.compile(multi_line_content_rex, re.RegexFlag.DOTALL)
+        re_replace(text, multi_line_comment_re, multi_line_content_re)

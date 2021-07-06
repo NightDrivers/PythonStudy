c642fbd2fc0cf3d52e56b5ee4bae630c20427888
NightDriver <lin_de_chun@sina.com>
2021-07-06 14:42:53 +0800

    1、mac驱动认证工具修改

--- script/mac_driver_notarize.py
+++ script/mac_driver_notarize.py
@@ -4,7 +4,7 @@
 import argparse
 import os
 import shutil
-from script import ShellCommand
+# from script import ShellCommand
 import subprocess
 import re
 import time
@@ -19,6 +19,19 @@ developer_password = "solr-vnoz-tzpu-pdmr"
 developer_team = "976NLVLZMW"
 
 
+def excute_shell(command: str, verbose: bool = False):
+    params = {"text": True}
+    program = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, **params)
+    outputs = []
+    while program.poll() is None:
+        # 当程序退出时，似乎会读取到一个空字符串
+        item = program.stdout.readline()
+        if verbose:
+            print(item, end="")
+        outputs.append(item)
+    return program.returncode, "".join(outputs)
+
+
 def parse_argv():
     __parse = argparse.ArgumentParser()
     __parse.add_argument("--identity", required=True, help="包id")
@@ -29,6 +42,8 @@ def parse_argv():
     __parse.add_argument("--developer_account", help="开发账号")
     __parse.add_argument("--developer_password", help="开发账号密码")
     __parse.add_argument("--developer_team", help="开发团队id")
+    __parse.add_argument("--skip-rebuild-filter", help="是否重新编译filter文件夹", action="store_true")
+    __parse.add_argument("--skip-notarize", help="是否公证pkg文件，测试使用", action="store_true")
 
     return __parse.parse_args()
 
@@ -40,7 +55,7 @@ def get_notarize_info(request_uuid: str, function):
                           "--password {2}"
     notarize_result_cmd = notarize_result_cmd.format(request_uuid, developer_name, developer_password)
     print("查询认证结果")
-    flag, result = ShellCommand.excute_shell(notarize_result_cmd, verbose=True)
+    flag, result = excute_shell(notarize_result_cmd, verbose=True)
     if flag != 0:
         print("认证信息查询失败")
         exit(-1)
@@ -68,7 +83,7 @@ def staple_ticket(package: str, notarize_info: dict):
     # 打开安装包目录
     os.system("open {0}".format(notarize_info["LogFileURL"]))
     cmd = "xcrun stapler staple {0}".format(package)
-    flag, result = ShellCommand.excute_shell(cmd, verbose=True)
+    flag, result = excute_shell(cmd, verbose=True)
     if flag != 0:
         exit(-1)
 
@@ -101,16 +116,22 @@ if __name__ == '__main__':
 
     home = subprocess.getoutput("pwd")
 
-    src_dir = home + "/filter"
-    for src in src_dir:
-        item_path = "{0}/{1}".format(src_dir, src)
-        if os.path.isdir(item_path):
-            flag, result = ShellCommand.excute_shell("cd {0};make clean;make;make install")
-            if flag != 0:
-                print("{0} make clean;make;make install 失败".format(item_path))
-                exit(-1)
+    if not argv.skip_rebuild_filter:
+        src_dir = home + "/filter"
+        for src in os.listdir(src_dir):
+            item_path = "{0}/{1}".format(src_dir, src)
+            print(item_path)
+            if os.path.isdir(item_path):
+                cmd = "cd {0};make clean;make".format(item_path)
+                print(cmd)
+                flag, result = excute_shell(cmd)
+                if flag != 0:
+                    print("{0} 失败".format(cmd))
+                    exit(-1)
 
-    flag, result = ShellCommand.excute_shell("make clean;make build")
+    cmd = "make clean;make build"
+    print(cmd)
+    flag, result = excute_shell(cmd)
     if flag != 0:
         print("{0} make clean;make build 失败".format(home))
         exit(-1)
@@ -119,7 +140,9 @@ if __name__ == '__main__':
 
     for item in os.listdir(ppd_dir):
         if item.endswith(".ppd"):
-            os.system("gzip {0}/{1}".format(ppd_dir, item))
+            cmd = "gzip {0}/{1}".format(ppd_dir, item)
+            print(cmd)
+            os.system(cmd)
 
     code_dir = "{0}/roots/usr/libexec/cups/filter".format(home)
     for item in os.listdir(code_dir):
@@ -128,7 +151,8 @@ if __name__ == '__main__':
         item_path = code_dir + "/" + item
 
         cmd = "chmod 755 {0}".format(item_path)
-        flag, result = ShellCommand.excute_shell(cmd, verbose=True)
+        print(cmd)
+        flag, result = excute_shell(cmd, verbose=True)
         if flag != 0:
             print("{0}文件权限修改失败".format(item))
             exit(-1)
@@ -136,20 +160,28 @@ if __name__ == '__main__':
         cmd = "codesign --force --verify --verbose " \
               "--sign \"{0}\" " \
               "{1} --deep --options runtime --timestamp"
+        print(cmd)
         cmd = cmd.format(codesign_identity, item_path)
-        flag, result = ShellCommand.excute_shell(cmd, verbose=True)
+        flag, result = excute_shell(cmd, verbose=True)
         if flag != 0:
             print("{0}代码签名失败".format(item))
             exit(-1)
 
     # 给脚本文件添加权限
-    script_path = "{0}/scripts"
+    script_path = "{0}/scripts".format(home)
     for item in os.listdir(script_path):
+        if item.startswith("."):
+            continue
         script_dir = script_path + "/" + item
-        for script_item in script_dir:
+        print(script_dir)
+        for script_item in os.listdir(script_dir):
+            if script_item.startswith("."):
+                continue
             item_path = script_dir + "/" + script_item
+            print(item_path)
             cmd = "chmod 755 {0}".format(item_path)
-            flag, result = ShellCommand.excute_shell(cmd, verbose=True)
+            print(cmd)
+            flag, result = excute_shell(cmd, verbose=True)
             if flag != 0:
                 print("{0}文件权限修改失败".format(item))
                 exit(-1)
@@ -162,7 +194,8 @@ if __name__ == '__main__':
                   "--ownership recommended " \
                   "build/{2}.pkg"
     install_cmd = install_cmd.format(identity, version, bundle_name)
-    flag, result = ShellCommand.excute_shell(install_cmd, verbose=True)
+    print(install_cmd)
+    flag, result = excute_shell(install_cmd, verbose=True)
     if flag != 0:
         print("Install包导出失败")
         exit(-1)
@@ -175,7 +208,8 @@ if __name__ == '__main__':
                     "--ownership recommended " \
                     "build/uninstall-{2}.pkg"
     uninstall_cmd = uninstall_cmd.format(identity, version, bundle_name)
-    flag, result = ShellCommand.excute_shell(uninstall_cmd, verbose=True)
+    print(uninstall_cmd)
+    flag, result = excute_shell(uninstall_cmd, verbose=True)
     if flag != 0:
         print("Uninstall包导出失败")
         exit(-1)
@@ -190,33 +224,36 @@ if __name__ == '__main__':
                   "build/{3}-v{1}.pkg"
     package_cmd = package_cmd.format(identity, version, install_sign_identity, bundle_name)
     print(package_cmd)
-    flag, result = ShellCommand.excute_shell(package_cmd, verbose=True)
+    flag, result = excute_shell(package_cmd, verbose=True)
     if flag != 0:
         print("打包失败")
         exit(-1)
 
-    notarize_cmd = "xcrun altool --notarize-app " \
-                   "--primary-bundle-id {0} " \
-                   "--username {1} " \
-                   "--password {2} " \
-                   "--file build/{3}-v{4}.pkg " \
-                   "-itc_provider {5} &> tmp"
-    notarize_cmd = notarize_cmd.format(identity, developer_name, developer_password, bundle_name, version, developer_team)
-    print(notarize_cmd)
-    flag, result = ShellCommand.excute_shell(notarize_cmd, verbose=True)
-    if flag != 0:
-        print("包认证失败")
-        exit(-1)
+    if not argv.skip_notarize:
+        notarize_cmd = "xcrun altool --notarize-app " \
+                       "--primary-bundle-id {0} " \
+                       "--username {1} " \
+                       "--password {2} " \
+                       "--file build/{3}-v{4}.pkg " \
+                       "-itc_provider {5} &> tmp"
+        notarize_cmd = notarize_cmd.format(identity, developer_name, developer_password, bundle_name, version,
+                                           developer_team)
+        print(notarize_cmd)
+        flag, result = excute_shell(notarize_cmd, verbose=True)
+        if flag != 0:
+            print("包认证失败")
+            exit(-1)
 
-    file = open("tmp")
-    request_uuid = ""
-    for item in file:
-        if item.startswith("RequestUUID = "):
-            items = item.split(" = ")
-            print(items)
-            request_uuid = items[1][:-1]
-    if len(request_uuid) == 0:
-        print("RequestUUID not found in tmp")
-        exit(-1)
+        file = open("tmp")
+        request_uuid = ""
+        for item in file:
+            if item.startswith("RequestUUID = "):
+                items = item.split(" = ")
+                print(items)
+                request_uuid = items[1][:-1]
+        if len(request_uuid) == 0:
+            print("RequestUUID not found in tmp")
+            exit(-1)
 
-    get_notarize_info(request_uuid, lambda info: staple_ticket("build/{0}-v{1}.pkg".format(bundle_name, version), info))
+        get_notarize_info(request_uuid,
+                          lambda info: staple_ticket("build/{0}-v{1}.pkg".format(bundle_name, version), info))
